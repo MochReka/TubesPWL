4f07a5b6906cdc11f18089408f9cdbca35d2d9da
MochReka <mochreka87@gmail.com>
2023-11-03 21:44:49 +0700

    navbar

---
+++ resources/views/tampil.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite ('resources/css/app.css')
+    <style>
+        .flex-container {
+            display: flex;
+        }
+
+        .sidebar {
+            width: 20%; /* Sesuaikan lebar sidebar sesuai kebutuhan */
+        }
+
+        .content {
+            width: 105%; /* Sisakan ruang untuk konten utama */
+        }
+    </style>
+</head>
+<body>
+    <div class="navbar">
+        <x-navbar/>
+    </div>
+    <div class="flex-container">
+        <div class="sidebar">
+            <x-sidebar/>    
+        </div>
+        <div class="content">
+            <div class="p-2 m-3 border-spacing-8">
+                <x-tabel/>    
+            </div>
+        </div>
+    </div>
+</body>
+<div class="bottom-100">
+    <x-footer/>
+</div>
+</html>
